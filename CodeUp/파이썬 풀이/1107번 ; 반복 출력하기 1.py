ebf48a9d0b673b10f6fef1ad78a96c350b5655e3
bright-night-sky <bright_night_sky@naver.com>
2021-08-12 23:04:36 +0900

    2021.08.12 CodeUp Problem Solving
    [CodeUp][Python] 1106번 : int의 범위
    [CodeUp][Python] 1107번 : 반복 출력하기 1
    [CodeUp][Python] 1108번 : 반복 출력하기 2

---
+++ CodeUp/파이썬 풀이/1107번 ; 반복 출력하기 1.py
@@ -0,0 +1,4 @@
+# https://codeup.kr/problem.php?id=1107
+
+# print 단어를 연속으로 50번 출력합니다.
+print('print' * 50)
---
+++ CodeUp/파이썬 풀이/1108번 ; 반복 출력하기 2.py
@@ -0,0 +1,5 @@
+# https://codeup.kr/problem.php?id=1108
+
+# hello 단어를 연속으로 20번 출력하고,
+# 한 칸 띄운 뒤, world 단어를 연속으로 30번 출력합니다.
+print('hello' * 20, 'world' * 30)
